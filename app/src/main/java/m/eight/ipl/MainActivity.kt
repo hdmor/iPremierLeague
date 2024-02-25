8f1a42c8c31e78b7297273155332fd278ff910ad
hdmor <hdmor.h@gmail.com>
2024-02-25 16:47:34 +0330

    First initialized project with required dependencies

---
+++ app/src/main/java/m/eight/ipl/MainActivity.kt
@@ -0,0 +1,24 @@
+package m.eight.ipl
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import m.eight.ipl.ui.theme.IPLTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IPLTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+
+                }
+            }
+        }
+    }
+}
